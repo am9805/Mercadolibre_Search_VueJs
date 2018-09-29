cb0e760fe1484e8568308de6b5e5414898632e19
Angelica <angelica.arroyavem@udea.edu.co>
2018-09-28 20:52:32 -0500

    The search works

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <SearchBar/>
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand">Mercadolibre Search</a>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="value">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="getValue">Search</button>
+      </form>
+    </nav>
     <ItemsList :object= 'info'/>
   
   </div>
 </template>
 
 <script>
-import SearchBar from './components/search_bar.vue'
 import ItemsList from './components/items_list.vue'
 import Axios from 'axios'
 
@@ -15,7 +20,8 @@ export default {
   name: 'app',
   data(){
     return{
-      info: []
+      info: [],
+      value: "xiaomi"
 
     }
   },
@@ -24,8 +30,16 @@ export default {
     ItemsList
   },
    mounted () {
-    Axios.get('https://api.mercadolibre.com/sites/MCO/search?q=iphone&limit=5')
+    Axios.get(`https://api.mercadolibre.com/sites/MCO/search?q=${this.value}&limit=5`)
       .then(response => (this.info = response.data.results))
+  },
+  methods:{
+    getValue(e) {
+        e.preventDefault();
+        Axios.get(`https://api.mercadolibre.com/sites/MCO/search?q=${this.value}&limit=5`)
+      .then(response => (this.info = response.data.results))
+
+     }
   }
 
   
@@ -40,5 +54,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.bg-light {
+    background-color :#ffe600!important;
+}
+.btn-outline-success {
+    color: #2d3277;
+    background-color: transparent;
+    background-image: none;
+    border-color: #2d3277;
 }
 </style>
